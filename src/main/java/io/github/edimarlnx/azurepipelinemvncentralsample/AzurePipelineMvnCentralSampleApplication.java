f5f917590344f4d38ff28cfeeaac1e09bf9182da
Edimar Cardoso <edimar.cardoso@tecsinapse.com.br>
2019-04-05 19:34:55 -0300

    Init

---
+++ src/main/java/io/github/edimarlnx/azurepipelinemvncentralsample/AzurePipelineMvnCentralSampleApplication.java
@@ -0,0 +1,13 @@
+package io.github.edimarlnx.azurepipelinemvncentralsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurePipelineMvnCentralSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurePipelineMvnCentralSampleApplication.class, args);
+	}
+
+}
