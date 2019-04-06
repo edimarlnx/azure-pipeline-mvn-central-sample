f23f324d4913b1e3ffb55bbd4703b17780e397e4
Edimar Cardoso <edimar.cardoso@tecsinapse.com.br>
2019-04-06 00:20:31 -0300

    Test Azure Pipeline

--- src/main/java/io/github/edimarlnx/azurepipelinemvncentralsample/AzurePipelineMvnCentralSampleApplication.java
+++ src/main/java/io/github/edimarlnx/azurepipelinemvncentralsample/AzurePipelineMvnCentralSampleApplication.java
@@ -1,13 +1,11 @@
 package io.github.edimarlnx.azurepipelinemvncentralsample;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+//@SpringBootApplication
 public class AzurePipelineMvnCentralSampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AzurePipelineMvnCentralSampleApplication.class, args);
+		//SpringApplication.run(AzurePipelineMvnCentralSampleApplication.class, args);
+		System.out.println("App Teste");
 	}
 
 }
